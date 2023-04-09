330000fe05c25022219ec61758be128bc0c0f17e
Thomas Hoffmann Kilbak <thomaskilbak@gmail.com>
2023-04-09 14:08:43 +0200

    Add working script for part 1

--- assignment1/part-1/connect.js
+++ assignment1/part-1/connect.js
@@ -1,22 +1,5 @@
 const { spawn } = require("child_process")
-
-async function doStuff() {
-    const nodeId = process.argv[2]
-    const sshProcess = spawn("ssh", ["-L", `20000:pycom-${nodeId}:20000`, "itu2023iot2@strasbourg.iot-lab.info"])
-    sshProcess.on("error", () => console.log("ssh process broke"))
-    sshProcess.on("exit", () => console.log("ssh process closed"))
-    await sleep(1000)
-
-    const socatProcess = spawn("socat", ["-d", "-v", "PTY,link=/tmp/ttyPYC0,crnl,echo=0", "TCP:localhost:20000"])
-    socatProcess.on("error", () => console.log("socat process broke"))
-    socatProcess.on("exit", () => console.log("socat process closed"))
-    await sleep(1000)
-    
-    spawn('rshell', ["-f ./rshell_commands.txt"], {
-        stdio: 'inherit',
-        shell: true
-    })
-}
+const fs = require("fs")
 
 function sleep(ms) {
     return new Promise((resolve) => {
@@ -24,4 +7,65 @@ function sleep(ms) {
     });
 }
 
-doStuff()
+function createDupes() {
+    if (!fs.existsSync(`./macs`)) {
+        fs.mkdirSync("./macs")
+    }
+    for (let i = 0; i < 10; i++) {
+        if (!fs.existsSync(`./macs/${i}`)) {
+            fs.mkdirSync(`./macs/${i}`)
+        }
+        let file = fs.readFileSync("main.py", "utf-8").split("\n")
+        file[7] = `mac = "80${i}abcdef0abcdef"`
+        fs.writeFileSync(`./macs/${i}/main.py`, file.join("\n"))
+    }
+}
+
+async function deploy(nodeIds) {
+    const connList = []
+    for (let i = 0; i < nodeIds.length; i++) {
+        connList.push("-L")
+        connList.push(`2000${i}:pycom-${nodeIds[i]}:20000`)
+    }
+
+    console.log("ssh")
+    const sshProcess = spawn("ssh", [...connList, "itu2023iot2@strasbourg.iot-lab.info"])
+    sshProcess.on("error", () => console.log("ssh process broke"))
+    sshProcess.on("exit", () => console.log("ssh process closed"))
+    await sleep(3000)
+
+    for (let i = 0; i < nodeIds.length; i++) {
+        console.log("socat:", i)
+        const socatProcess = spawn("socat", ["-d", "-v", `PTY,link=/tmp/ttyPYC${i},crnl,echo=0`, `TCP:localhost:2000${i}`])
+        socatProcess.on("error", () => console.log(`socat process ${i} broke`))
+        socatProcess.on("exit", () => console.log(`socat process ${i} closed`))
+        await sleep(1000)
+    }
+
+    createDupes()
+
+    let rshellCommands = []
+    for (let i = 0; i < nodeIds.length; i++) {
+        rshellCommands.push(`connect serial /tmp/ttyPYC${i}`)
+    }
+    rshellCommands.push("cp macs/0/main.py /pyboard/flash/main.py")
+    
+    for (let i = 2; i <= nodeIds.length; i++) {
+        rshellCommands.push(`cp macs/${i-1}/main.py /pyboard-${i}/flash/main.py`)
+    }
+
+    fs.writeFileSync("commands.txt", rshellCommands.join("\n"))
+    console.log("rshell")
+    const rshellProcess = spawn('rshell', ["-f ./commands.txt"], {
+        stdio: 'inherit',
+        shell: true
+    })
+    rshellProcess.on("error", () => console.log("rshell process broke"))
+    rshellProcess.on("exit", () => {
+        fs.rmdirSync("./macs", {recursive: true})
+        fs.unlinkSync("./commands.txt")
+        console.log("rshell process closed")
+    })
+}
+
+deploy([1,2,5,6,10])
--- assignment1/part-1/main.py
+++ assignment1/part-1/main.py
@@ -1,16 +1,18 @@
-import machine
-import time
-import ubinascii
-from network import LoRa
 import socket
+from network import LoRa
+import ubinascii
+import time
+import machine
 
 print("starting")
+mac = ""
+dev_eui = ubinascii.unhexlify(mac)
 
 lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
 app_eui = ubinascii.unhexlify('0000000000000000')
 app_key = ubinascii.unhexlify('1257279ab3b44855ad3260a6c3123f74')
 # mac address of my Pycom, such that the one on IoT lab can also access LoRa
-dev_eui = ubinascii.unhexlify("70b3d5499f3b3b80")
+# dev_eui = ubinascii.unhexlify("70b3d5499f3b3b80")
 lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
 while not lora.has_joined():
     time.sleep(1)
