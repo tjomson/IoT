8ea70b55b02534183a9e945de1359a8c352a0ebb
Thomas Hoffmann Kilbak <thki@queue-it.com>
2023-04-25 15:41:13 +0200

    LED blink stuff

--- led-blink/main.py
+++ led-blink/main.py
@@ -1,12 +1,18 @@
 import pycom
 import time
-
+import machine
+from machine import Pin
 pycom.heartbeat(False)
 
-while True:
-    pycom.rgbled(0xFF0000)  # Red
-    time.sleep(1)
-    pycom.rgbled(0x00FF00)  # Green
-    time.sleep(1)
-    pycom.rgbled(0x0000FF)  # Blue
-    time.sleep(1)
+# wake_pin = Pin('P11', mode=Pin.IN, pull=Pin.PULL_UP)
+# machine.pin_sleep_wakeup([wake_pin], machine.WAKEUP_ALL_LOW, enable_pull=True)
+
+# while True:
+pycom.rgbled(0x880000)  # Red
+time.sleep(1)
+pycom.rgbled(0x008800)  # Green
+time.sleep(1)
+pycom.rgbled(0x000088)  # Blue
+time.sleep(1)
+
+machine.deepsleep(5000)
